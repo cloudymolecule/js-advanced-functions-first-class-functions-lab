b3a729c96ff7d426e99c37676de7dace8760becf
Cloudymolecule <baires82@gmail.com>
2021-02-25 14:56:23 -0500

    commit tree 8c8ff273bdfcaed8c27f0134d55a4d123763235b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    return array.slice(0, 2)
+}
+
+function returnLastTwoDrivers(array){
+    return array.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(fare){
+    const myFunc = function(num){
+        return (fare * num)
+    }
+    return myFunc
+}
+
+function fareDoubler(fare){
+    const myVar = createFareMultiplier(fare)
+    return myVar(2)
+}
+
+function fareTripler(fare){
+    const myVar = createFareMultiplier(fare)
+    return myVar(3)
+}
+
+function selectDifferentDrivers(drivers, aFunc){
+    return aFunc(drivers)
+}
